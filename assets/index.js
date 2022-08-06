af8efe9084cd1ed906ac51707eb2150717958aa3
Pablo Amil <pablo.amil91@gmail.com>
2022-08-05 23:17:10 -0300

    limpieza de comentarios

--- assets/index.js
+++ assets/index.js
@@ -30,9 +30,6 @@ const products = [{
   },
 ];
 
-
-
-
 const productsEl = document.querySelector('.products');
 const cartItemsEl = document.querySelector('.cart-items');
 const subTotalEl = document.querySelector('.subTotal');
@@ -44,13 +41,11 @@ const toDark = document.getElementById('toDark')
 
 
 
-
-
-let cart = JSON.parse(localStorage.getItem('CART')) || []; // si no hay nada en localStorage le da como valor un array vacio
+let cart = JSON.parse(localStorage.getItem('CART')) || []; 
 actualizarCarrito();
 
 function renderizarProducts() {
-  products.forEach((product) => {   /// revisar que item-container tiene 2 clases pero mal puestas
+  products.forEach((product) => {   
     productsEl.innerHTML += `
     <div class="item">
     <div class="item-container" class="item-flex"> 
@@ -70,22 +65,20 @@ function renderizarProducts() {
     </div>
 </div>
     `
-  }) // al hacer click llama a addToCart y le pasa el parametro product.id
+  }) 
 }
 renderizarProducts();
 
-function addToCart(id) { //////// esta funcion guarda el objeto especifico en el array //// este viene del onclick del <p> que llama a la funcion y pasa ese id como parametro. addToCart(id) representa al addToCart${product.id}
-
-  
+function addToCart(id) { 
 
   if (cart.some((item) => item.id === id)) {
-    callToast() // muestra mensaje utilizando toastify
+    callToast() 
   } else { 
     callToast2()
-    const item = products.find((product) => product.id === id); // busca en el array el objeto (el producto) que cumple con la condicion de tener un id igual al del onclick y LO RETORNA ENTERO(el id del producto tiene que ser igual que el parametro)
+    const item = products.find((product) => product.id === id); 
     cart.push({
       ...item,
-      cantidad: 1, // se utiliza spread para agregar una propiedad y valor nuevos 
+      cantidad: 1, 
     })
   }
   actualizarCarrito()
@@ -94,11 +87,11 @@ function addToCart(id) { //////// esta funcion guarda el objeto especifico en el
 function actualizarCarrito() {
   renderizarItemsCarrito();
   renderizarSubTotal();
-  localStorage.setItem('CART', JSON.stringify(cart)); // cada vez que se actualiza el carrito manda al CART el carrito con su contenido actual
+  localStorage.setItem('CART', JSON.stringify(cart)); 
 }
 
 function renderizarItemsCarrito() {
-  cartItemsEl.innerHTML = ""; // esto evita la repeticion de productos 
+  cartItemsEl.innerHTML = ""; 
   cart.forEach((item) => {
     cartItemsEl.innerHTML += `
     <div class="cart-item"> 
@@ -126,19 +119,19 @@ function elegirUnidades(acc, id) {
   cart = cart.map((item) => {
     let unidadesEnCarrito = item.cantidad
     if (item.id === id) {
-      if (acc === 'restar' && unidadesEnCarrito > 1) { // incrementa las unidades solo si es mayor a 1, evitando los numeros negativos
+      if (acc === 'restar' && unidadesEnCarrito > 1) { 
         unidadesEnCarrito--
       } else if (acc === 'sumar') {
         unidadesEnCarrito++
       }
     };
     return {
-      ...item, // devuelve el objeto y lo desestructura nuevamente agregandole el nuevo valor
+      ...item, 
       cantidad: unidadesEnCarrito,
       
     };
   })
-  actualizarCarrito(); // actualiza el carrito nuevamente 
+  actualizarCarrito(); 
 };
 
 
@@ -149,7 +142,7 @@ function renderizarSubTotal() {
     precioTotal += item.precio * item.cantidad;
     totalDeItems += item.cantidad;
   })
-  subTotalEl.innerHTML = ''; // lo actualiza a 0 para no repetir y vuelve a escribir el resultado
+  subTotalEl.innerHTML = ''; 
   subTotalEl.innerHTML += `
   Subtotal: $${precioTotal}
   <input type="button" value="Comprar!" onclick="realizarCompra()">
@@ -157,17 +150,15 @@ function renderizarSubTotal() {
   contadorDeItems.innerHTML = `<p>${totalDeItems}</p>`
 };
 
-
-function removeFromCart(id) { // un parametro para saber a quien remover
-  cart = cart.filter((item) => item.id !== id) // reemplaza el array de cart con uno nuevo si es que el id pasado como parametro es distinto
+function removeFromCart(id) { 
+  cart = cart.filter((item) => item.id !== id) 
   actualizarCarrito();
 };
-
 
 function realizarCompra() {
   cartItemsEl.innerHTML = ``;
   subTotalEl.innerHTML = `<p>Subtotal: $</p>`;
-  Swal.fire({ // sweetalert para mostrar mensaje de confirmacion 
+  Swal.fire({ 
     position: 'center',
     icon: 'success',
     title: 'Gracias por tu compra!',
@@ -179,8 +170,8 @@ function realizarCompra() {
   actualizarCarrito();
 };
 
+////fetch////
 
-// utilizacion de fetch para obtener datos sobre otros productos y generar nueva seccion
 fetch("./assets/products.json")
   .then((resp) => resp.json())
   .then((productos) => {
@@ -210,11 +201,11 @@ function turnToDark() {
   document.body.classList.toggle('dark');
   document.body.classList.contains('dark') ? localStorage.setItem('darkMode', 'true') : (localStorage.setItem('darkMode', 'false'));
 };
-if (localStorage.getItem('darkMode') === 'true') { // si quedo grabado en storage
+if (localStorage.getItem('darkMode') === 'true') { 
   document.body.classList.add('dark')
 };
 
-//// llamar toastify ////
+//// toastify ////
 function callToast() {
   Toastify({
     text: "Ya esta en el carrito",
